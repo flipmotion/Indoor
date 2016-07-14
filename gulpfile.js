b1f29b83d32f8ab4b5e6b0c3fa673ffb5219766b
igor <flipmotions@gmail.com>
2016-07-14 20:27:43 +0300

    Up text page

--- gulpfile.js
+++ gulpfile.js
@@ -18,10 +18,10 @@ gulp.task('styles', function() {
 	return gulp.src('build/less/style.less')
 	.pipe(less())
 	.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-	/*.pipe(rename({
+	.pipe(rename({
 	  suffix: '.min'
-	}))*/
-	//.pipe(minifycss())
+	}))
+	.pipe(minifycss())
 	.pipe(gulp.dest('assets/css'))
 	.pipe(connect.reload())
 	.pipe(notify({
@@ -59,9 +59,7 @@ gulp.task('html', function () {
 //js
 gulp.task('scripts', function() {
 	return gulp.src('build/js/my/*.js')
-		.pipe(babel({
-			presets: ['es2015']
-		}))
+		.pipe(babel())
 		.pipe(jshint())
 		.pipe(jshint.reporter('default'))
 		.pipe(concat('app.js'))
